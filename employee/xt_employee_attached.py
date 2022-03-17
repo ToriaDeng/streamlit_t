ccd532579cbdb9c8b9250944c32dc90164ebb397
ToriaDeng <101687112+ToriaDeng@users.noreply.github.com>
2022-03-16 21:43:33 -0700

    Add files via upload

---
+++ employee/xt_employee_attached.py
@@ -0,0 +1,64 @@
+import sqlite3
+conn = sqlite3.connect('data.db',check_same_thread=False)
+c = conn.cursor()
+
+
+def create_table():
+	c.execute('CREATE TABLE IF NOT EXISTS t_empoyeetable(Employee_Name TEXT,Position TEXT,Email_Address TEXT,Entry_date DATE)')
+
+
+def add_data(Name,Positions,Email,Entry_date):
+	c.execute('INSERT INTO t_empoyeetable(Employee_Name,Position,Email_Address,Entry_date) VALUES (?,?,?,?)',(Name,Positions,Email,Entry_date))
+	conn.commit()
+
+
+def view_all_data():
+	c.execute('SELECT * FROM  t_empoyeetable')
+	data = c.fetchall()
+	return data
+
+def view_all_employee_info():
+	c.execute('SELECT DISTINCT Employee_Name FROM t_empoyeetable')
+	data = c.fetchall()
+	return data
+
+def get_name(name):
+	c.execute('SELECT * FROM t_empoyeetable WHERE Employee_Name="{}"'.format(name))
+	data = c.fetchall()
+	return data
+
+
+def edit_info_data(new_name,new_position_status,new_email,new_entry_date,Name,Position,Email,Entry_date):
+	c.execute("UPDATE t_empoyeetable SET Employee_Name =?,Position=?,Email_Address=?,Entry_date=? WHERE Employee_Name =? and Position=?and Email_Address=? and Entry_date=? ",(new_name,new_position_status,new_email,new_entry_date,Name,Position,Email,Entry_date))
+	conn.commit()
+	data = c.fetchall()
+	return data
+
+def delete_data(name1):
+	c.execute('DELETE FROM t_empoyeetable WHERE Employee_Name="{}"'.format(name1))
+	conn.commit()
+
+# def view_all_email_info():
+# 	c.execute('SELECT DISTINCT Email_Address FROM t_empoyeetable')
+# 	data = c.fetchall()
+# 	return data
+
+# def get_email(email1):
+# 	c.execute('SELECT * FROM t_empoyeetable WHERE Email_Address="{}"'.format(email1))
+# 	data = c.fetchall()
+# 	return data
+
+def view_s_position():
+	c.execute("SELECT * FROM t_empoyeetable WHERE Position='Senior'")
+	data = c.fetchall()
+	return data
+
+def view_m_position():
+	c.execute("SELECT * FROM t_empoyeetable WHERE Position='Middle'")
+	data = c.fetchall()
+	return data
+
+def view_j_position():
+	c.execute("SELECT * FROM t_empoyeetable WHERE Position='Junior'")
+	data = c.fetchall()
+	return data
---
+++ gasprice/xt-gasprice.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+from random import gauss
+import re
+#from winreg import REG_OPTION_CREATE_LINK
+import requests
+import numpy as np
+import pandas as pd
+import streamlit as st
+import streamlit.components.v1 as components
+from pyecharts import options as opts
+from pyecharts.charts import Map
+from pyecharts.datasets import register_url
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud,STOPWORDS
+
+
+
+@st.cache(allow_output_mutation=True)
+def get_html_coviddata():
+    register_url("https://echarts-maps.github.io/echarts-countries-js/")
+    headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+          'cache-control': 'max-age=0',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
+          }
+    url = 'https://gasprices.aaa.com/state-gas-price-averages/'
+    resp = requests.get(url, headers=headers)
+    data = pd.read_html(resp.text)[0]
+    
+    cols = ['Regular', 'Mid-Grade','Premium','Diesel']
+    data[cols] = data[cols].replace(r'[$,]', '', regex=True).astype('float64')
+    return data
+
+
+def draw_map(data, col_name):
+    data = data.copy()  
+    col = col_name.title().replace(' ', '')
+    data[col] = data[col].map(lambda x: np.nan if pd.isna(x) else float(x))
+    max_, min_ = data[col].max(), data[col].min()
+    c = (
+        Map()
+        .add(col_name, data[['State', col]].values, '美国', is_map_symbol_show=False)
+        .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title=f"Gas Price in U.S ({col_name}) : {datetime.today().strftime('%m-%d-%Y')}"),
+            visualmap_opts=opts.VisualMapOpts(max_=max_, min_=min_, precision=True),
+            legend_opts=opts.LegendOpts(selected_mode='single',
+                                       pos_top='bottom')
+        )
+    )
+    return c.render_embed()
+
+## Page expands to full width
+st.set_page_config(layout="wide")
+
+# Title
+st.title("U.S Today's Gasprice")
+st.markdown("""
+**Data source:** [AAA GAS PRICE](https://gasprices.aaa.com/state-gas-price-averages/)
+
+""")
+
+
+# get_covid_data for usa
+price_data = get_html_coviddata()
+
+
+# selected box
+choice = ['Regular', 'Mid-Grade', 'Premium', 'Diesel']
+choice_selected = st.selectbox("Select Gas Type", choice)
+st.write(choice_selected)
+# Draw Map
+components.html(draw_map(price_data, choice_selected), width=3000, height=600)
+
+st.write('---')
+
+
+
+#layout
+col1,col2 = st.columns(2)
+
+#Show the top gas price in selected gas type
+def price_plot(data, column, num):
+    data = data.copy()
+    plt.figure(figsize=(5,7))
+    plt.subplots_adjust(top=1, bottom=0)
+    data = data[['State', column]].sort_values(by=column, ascending=False)[:num].copy()
+    plt.barh(data['State'], data[column], label=column)
+    plt.xlabel('$ Dallor')
+    plt.ylabel('States')
+    plt.legend(loc='upper right')
+    return plt
+
+col1.subheader('Show the top gas price in selected gas type')
+num_states = col1.slider('Slider to get numbers of top gas price in states', 1, 50, 15)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+col1.pyplot(price_plot(price_data, choice_selected, num_states))
+
+
+#wordcloud for seletced gas type
+col2.subheader("Today's Gas Price")
+price_data1=price_data.copy()
+col2.write(price_data1)
+price_data1= price_data1[['State',choice_selected]]
+dict = {} 
+for col in price_data1.values:
+    dict[col[0]] = col[1]
+wordcloud = WordCloud(background_color = 'White', width=1920,height=1080)
+wordcloud.generate_from_frequencies(dict)
+plt.figure(figsize=(5,8))
+plt.imshow(wordcloud,interpolation='bilinear')
+plt.axis('off')
+col2.subheader('Wordcloud for Selected Gas Price')
+col2.pyplot(plt)
+
+col2.write('---')
