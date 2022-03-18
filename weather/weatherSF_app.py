1e1a67fd125a7b269030fb9bbfec557c361b99ea
ToriaDeng <101687112+ToriaDeng@users.noreply.github.com>
2022-03-17 22:25:55 -0700

    Update weatherSF_app.py

--- weather/weatherSF_app.py
+++ weather/weatherSF_app.py
@@ -18,7 +18,7 @@ st.set_page_config(layout="wide")
 
 # image = Image.open('sf-.jpg')
 
-st.image('sf-.jpg', width = 700)
+st.image('./sf-.jpg', width = 700)
 
 st.title('San Francisco 15-Day Weather Forecast App')
 st.markdown("""
